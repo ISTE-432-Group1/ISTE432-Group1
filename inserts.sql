5d71c9cc12982fb8d9c468c896c695440dc6397b
Da7238 <60263576+Da7238@users.noreply.github.com>
2022-02-25 14:33:50 -0500

    Created inserts for subject and type tables.

---
+++ inserts.sql
@@ -0,0 +1,26 @@
+INSERT INTO type (typeID, name) VALUES ('A', 'Autobiography');
+INSERT INTO type (typeID, name) VALUES ('B', 'Biography');
+INSERT INTO type (typeID, name) VALUES ('C', 'Compilation');
+INSERT INTO type (typeID, name) VALUES ('D', 'Diary/Journal');
+INSERT INTO type (typeID, name) VALUES ('F','Fictional');
+INSERT INTO type (typeID, name) VALUES ('G', 'Gallows broadside');
+INSERT INTO type (typeID, name) VALUES ('L', 'Letters/Correspondence');
+INSERT INTO type (typeID, name) VALUES ('M', 'Memoir');
+
+INSERT INTO subject(subjectID, name) VALUES ('A', 'Adventure');
+INSERT INTO subject(subjectID, name) VALUES ('C', 'Criminal');
+INSERT INTO subject(subjectID, name) VALUES ('CY', 'Celebrity');
+INSERT INTO subject(subjectID, name) VALUES ('D', 'Domestic');
+INSERT INTO subject(subjectID, name) VALUES ('G', 'Great Man');
+INSERT INTO subject(subjectID, name) VALUES ('H', 'Historical');
+INSERT INTO subject(subjectID, name) VALUES ('L', 'Literary Memoir');
+INSERT INTO subject(subjectID, name) VALUES ('M', 'Military');
+INSERT INTO subject(subjectID, name) VALUES ('MC', 'Middle-class');
+INSERT INTO subject(subjectID, name) VALUES ('P', 'Politics');
+INSERT INTO subject(subjectID, name) VALUES ('R', 'Religious');
+INSERT INTO subject(subjectID, name) VALUES ('S', 'Satire');
+INSERT INTO subject(subjectID, name) VALUES ('SC', 'Social Critique');
+INSERT INTO subject(subjectID, name) VALUES ('SD', 'School Days');
+INSERT INTO subject(subjectID, name) VALUES ('T', 'Travel');
+INSERT INTO subject(subjectID, name) VALUES ('TH', 'Theatre');
+
