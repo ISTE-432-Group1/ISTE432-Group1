b760655f41af3b0505eb4fc45f15db6097754caf
Darlene Ardila <da7238@rit.edu>
2022-03-23 15:31:10 -0400

    Added admin, editor, contributor, and public classes.

---
+++ src/data/Public.class.php
@@ -0,0 +1,21 @@
+<?php 
+
+class PublicUser {
+    private $userName;
+    private $role;
+    private $password;
+    private $accountType;
+
+    function __construct($userName="", $role="", $password="", $accountType="") {
+        $this->userName=$userName;
+        $this->role=$role;
+        $this->password=$password;
+        $this->accountType=$accountType;
+    }
+
+    function getUserName() {return $this->userName;}
+    function getRole() {return $this->role;}
+    function getpassword() {return $this->password;}
+    function getAccountType() {return $this->accountType;}
+   
+}//Public class
